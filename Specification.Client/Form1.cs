11d65524a484d1ba4d0e2844cbf76c9cd431c88b
Алексей Ефимчик <vip.efimchik@mail.ru>
2020-09-14 20:09:29 +0700

    Initial Commit

--- Specification.Client/Form1.cs
+++ Specification.Client/Form1.cs
@@ -16,5 +16,10 @@ namespace Specification.Client
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
