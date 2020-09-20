082a89e29e9666b905bdbcd8c9a152caf19e1ea0
ImagineAngel <71269083+ImagineAngel@users.noreply.github.com>
2020-09-20 21:07:00 +0700

    the finally checking the form

--- Specification.Client/Form1.Designer.cs
+++ Specification.Client/Form1.Designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Imya = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Add_B = new System.Windows.Forms.Button();
             this.Update_B = new System.Windows.Forms.Button();
             this.Delete_B = new System.Windows.Forms.Button();
@@ -44,6 +40,10 @@
             this.Description_L = new System.Windows.Forms.Label();
             this.ID_L = new System.Windows.Forms.Label();
             this.Name_L = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Imya = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,14 +57,104 @@
             this.Description,
             this.ID,
             this.Imya});
-            this.dataGridView1.Location = new System.Drawing.Point(36, 38);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 31);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(657, 345);
+            this.dataGridView1.Size = new System.Drawing.Size(530, 279);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Add_B
+            // 
+            this.Add_B.Location = new System.Drawing.Point(27, 339);
+            this.Add_B.Name = "Add_B";
+            this.Add_B.Size = new System.Drawing.Size(137, 45);
+            this.Add_B.TabIndex = 2;
+            this.Add_B.Text = "Add";
+            this.Add_B.UseVisualStyleBackColor = true;
+            // 
+            // Update_B
+            // 
+            this.Update_B.Location = new System.Drawing.Point(206, 339);
+            this.Update_B.Name = "Update_B";
+            this.Update_B.Size = new System.Drawing.Size(137, 45);
+            this.Update_B.TabIndex = 3;
+            this.Update_B.Text = "Update";
+            this.Update_B.UseVisualStyleBackColor = true;
+            // 
+            // Delete_B
+            // 
+            this.Delete_B.Location = new System.Drawing.Point(383, 339);
+            this.Delete_B.Name = "Delete_B";
+            this.Delete_B.Size = new System.Drawing.Size(137, 45);
+            this.Delete_B.TabIndex = 4;
+            this.Delete_B.Text = "Delete";
+            this.Delete_B.UseVisualStyleBackColor = true;
+            // 
+            // Data_TB
+            // 
+            this.Data_TB.Location = new System.Drawing.Point(640, 96);
+            this.Data_TB.Name = "Data_TB";
+            this.Data_TB.Size = new System.Drawing.Size(155, 20);
+            this.Data_TB.TabIndex = 5;
+            // 
+            // Description_TB
+            // 
+            this.Description_TB.Location = new System.Drawing.Point(640, 132);
+            this.Description_TB.Name = "Description_TB";
+            this.Description_TB.Size = new System.Drawing.Size(155, 20);
+            this.Description_TB.TabIndex = 6;
+            // 
+            // ID_TB
+            // 
+            this.ID_TB.Location = new System.Drawing.Point(640, 168);
+            this.ID_TB.Name = "ID_TB";
+            this.ID_TB.Size = new System.Drawing.Size(155, 20);
+            this.ID_TB.TabIndex = 7;
+            // 
+            // Name_TB
+            // 
+            this.Name_TB.Location = new System.Drawing.Point(640, 204);
+            this.Name_TB.Name = "Name_TB";
+            this.Name_TB.Size = new System.Drawing.Size(155, 20);
+            this.Name_TB.TabIndex = 8;
+            // 
+            // Date_L
+            // 
+            this.Date_L.AutoSize = true;
+            this.Date_L.Location = new System.Drawing.Point(572, 99);
+            this.Date_L.Name = "Date_L";
+            this.Date_L.Size = new System.Drawing.Size(30, 13);
+            this.Date_L.TabIndex = 9;
+            this.Date_L.Text = "Date";
+            // 
+            // Description_L
+            // 
+            this.Description_L.AutoSize = true;
+            this.Description_L.Location = new System.Drawing.Point(572, 135);
+            this.Description_L.Name = "Description_L";
+            this.Description_L.Size = new System.Drawing.Size(60, 13);
+            this.Description_L.TabIndex = 10;
+            this.Description_L.Text = "Description";
+            // 
+            // ID_L
+            // 
+            this.ID_L.AutoSize = true;
+            this.ID_L.Location = new System.Drawing.Point(572, 171);
+            this.ID_L.Name = "ID_L";
+            this.ID_L.Size = new System.Drawing.Size(18, 13);
+            this.ID_L.TabIndex = 11;
+            this.ID_L.Text = "ID";
+            // 
+            // Name_L
+            // 
+            this.Name_L.AutoSize = true;
+            this.Name_L.Location = new System.Drawing.Point(572, 207);
+            this.Name_L.Name = "Name_L";
+            this.Name_L.Size = new System.Drawing.Size(35, 13);
+            this.Name_L.TabIndex = 12;
+            this.Name_L.Text = "Name";
             // 
             // Date
             // 
@@ -92,116 +182,14 @@
             this.Imya.HeaderText = "Name";
             this.Imya.MinimumWidth = 6;
             this.Imya.Name = "Imya";
-            this.Imya.Width = 120;
-            // 
-            // Add_B
-            // 
-            this.Add_B.Location = new System.Drawing.Point(36, 417);
-            this.Add_B.Margin = new System.Windows.Forms.Padding(4);
-            this.Add_B.Name = "Add_B";
-            this.Add_B.Size = new System.Drawing.Size(183, 55);
-            this.Add_B.TabIndex = 2;
-            this.Add_B.Text = "Add";
-            this.Add_B.UseVisualStyleBackColor = true;
-            // 
-            // Update_B
-            // 
-            this.Update_B.Location = new System.Drawing.Point(275, 417);
-            this.Update_B.Margin = new System.Windows.Forms.Padding(4);
-            this.Update_B.Name = "Update_B";
-            this.Update_B.Size = new System.Drawing.Size(183, 55);
-            this.Update_B.TabIndex = 3;
-            this.Update_B.Text = "Update";
-            this.Update_B.UseVisualStyleBackColor = true;
-            // 
-            // Delete_B
-            // 
-            this.Delete_B.Location = new System.Drawing.Point(511, 417);
-            this.Delete_B.Margin = new System.Windows.Forms.Padding(4);
-            this.Delete_B.Name = "Delete_B";
-            this.Delete_B.Size = new System.Drawing.Size(183, 55);
-            this.Delete_B.TabIndex = 4;
-            this.Delete_B.Text = "Delete";
-            this.Delete_B.UseVisualStyleBackColor = true;
-            // 
-            // Data_TB
-            // 
-            this.Data_TB.Location = new System.Drawing.Point(821, 118);
-            this.Data_TB.Margin = new System.Windows.Forms.Padding(4);
-            this.Data_TB.Name = "Data_TB";
-            this.Data_TB.Size = new System.Drawing.Size(205, 22);
-            this.Data_TB.TabIndex = 5;
-            // 
-            // Description_TB
-            // 
-            this.Description_TB.Location = new System.Drawing.Point(821, 162);
-            this.Description_TB.Margin = new System.Windows.Forms.Padding(4);
-            this.Description_TB.Name = "Description_TB";
-            this.Description_TB.Size = new System.Drawing.Size(205, 22);
-            this.Description_TB.TabIndex = 6;
-            // 
-            // ID_TB
-            // 
-            this.ID_TB.Location = new System.Drawing.Point(821, 207);
-            this.ID_TB.Margin = new System.Windows.Forms.Padding(4);
-            this.ID_TB.Name = "ID_TB";
-            this.ID_TB.Size = new System.Drawing.Size(205, 22);
-            this.ID_TB.TabIndex = 7;
-            // 
-            // Name_TB
-            // 
-            this.Name_TB.Location = new System.Drawing.Point(821, 251);
-            this.Name_TB.Margin = new System.Windows.Forms.Padding(4);
-            this.Name_TB.Name = "Name_TB";
-            this.Name_TB.Size = new System.Drawing.Size(205, 22);
-            this.Name_TB.TabIndex = 8;
-            // 
-            // Date_L
-            // 
-            this.Date_L.AutoSize = true;
-            this.Date_L.Location = new System.Drawing.Point(731, 122);
-            this.Date_L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Date_L.Name = "Date_L";
-            this.Date_L.Size = new System.Drawing.Size(38, 17);
-            this.Date_L.TabIndex = 9;
-            this.Date_L.Text = "Date";
-            // 
-            // Description_L
-            // 
-            this.Description_L.AutoSize = true;
-            this.Description_L.Location = new System.Drawing.Point(731, 166);
-            this.Description_L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Description_L.Name = "Description_L";
-            this.Description_L.Size = new System.Drawing.Size(79, 17);
-            this.Description_L.TabIndex = 10;
-            this.Description_L.Text = "Description";
-            // 
-            // ID_L
-            // 
-            this.ID_L.AutoSize = true;
-            this.ID_L.Location = new System.Drawing.Point(731, 210);
-            this.ID_L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.ID_L.Name = "ID_L";
-            this.ID_L.Size = new System.Drawing.Size(21, 17);
-            this.ID_L.TabIndex = 11;
-            this.ID_L.Text = "ID";
-            // 
-            // Name_L
-            // 
-            this.Name_L.AutoSize = true;
-            this.Name_L.Location = new System.Drawing.Point(731, 255);
-            this.Name_L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.Name_L.Name = "Name_L";
-            this.Name_L.Size = new System.Drawing.Size(45, 17);
-            this.Name_L.TabIndex = 12;
-            this.Name_L.Text = "Name";
+            this.Imya.Width = 130;
             // 
             // Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(1044, 494);
+            this.ClientSize = new System.Drawing.Size(817, 401);
             this.Controls.Add(this.Name_L);
             this.Controls.Add(this.ID_L);
             this.Controls.Add(this.Description_L);
@@ -214,7 +202,6 @@
             this.Controls.Add(this.Update_B);
             this.Controls.Add(this.Add_B);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
