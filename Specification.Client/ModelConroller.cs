e3358a5a597b3a8e2688a841fc76e86a63fff1bb
alexgvardin@gmail.com <alexgvardin@gmail.com>
2020-10-25 12:39:44 +0700

    Created Model Controller

---
+++ Specification.Client/ModelConroller.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Specification.Client.Data;
+using Specification.Client.Models;
+
+namespace Specification.Client
+{
+    public class ModelConroller
+    {
+        private ApplicationContext _context;
+
+        public ModelConroller()
+        {
+            _context = new ApplicationContext();
+        }
+
+        public List<Model> GetModels()
+        {
+            using (ApplicationContext _context = new ApplicationContext())
+            {
+                return _context.Models.ToList();
+            }
+        }
+
+        public (Model, Boolean) Add(Model model)
+        {
+            try
+            {
+                _context.Models.Add(model);
+                _context.SaveChanges();
+                return (model, true);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        public (Model, Boolean) Update(Model model)
+        {
+            try
+            {
+                Model modelContext = _context.Models.FirstOrDefault(m => m.Id == model.Id);
+
+                _context.Models.Remove(modelContext);
+                _context.SaveChanges();
+                return (model, true);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }     
+        }
+
+        public (int, Boolean) Remove(int Id)
+        {
+            try
+            {
+                Model modelContext = _context.Models.FirstOrDefault(m => m.Id == Id);
+
+                _context.Models.Remove(modelContext);
+                _context.SaveChanges();
+                return (Id, true);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+    }
+}
